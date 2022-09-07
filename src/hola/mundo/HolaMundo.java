3b18109a05e7f52fc3cb5270ae7a36e83a174391
Lucio Coudannes <luciocoudannes@gmail.com>
2022-09-06 21:47:35 -0300

    commit 1

---
+++ src/hola/mundo/HolaMundo.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hola.mundo;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author A288808
+ */
+public class HolaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       //ejercicio video 1
+        /*Scanner leer = new Scanner(System.in);
+                
+        String nombre;
+        
+        System.out.println("Ingresa tu nombre");
+        
+        nombre = leer.next();
+        
+        System.out.println("Hola Mundo! Soy " + nombre + " y estoy programando JAVA!" );
+        */
+        
+        //Ejercicio 1 Guia 6 Deteccion de errores
+         /*       
+        String nombre;
+        boolean bandera;
+        char Letra;*/
+         
+         //Ejercicio 2 Guia 6 Deteccion de errores
+         /*
+        int numero = 48;
+        double decimales = 3.55;
+        boolean bandera = false;*/
+         
+         //Ejercio 3 Guia 6
+         /*
+         int num1 = 2;
+         int num2 = 3;   
+         int resultado;
+         
+         resultado = num1 + num2;
+         
+         double division = num2 / num1;
+         
+         int resta = num2 - num1;*/
+         
+         String nombre = "Lucio";
+         
+         int edad;
+         
+         Scanner leer = new Scanner(System.in);
+         
+         System.out.println("Ingrese su edad");
+         
+         edad = leer.nextInt();
+         
+         System.out.println("Hola " + nombre + " tenes " + edad + " años");
+                        
+         
+         
+    }
+         
+                 
+         
+
+               
+        
+ }
+    
+
